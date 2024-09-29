9a0a73da19c1998141ee846e83aeda92b6d1898e
Matt Adam <95365935+minimatt25@users.noreply.github.com>
2024-09-29 19:28:53 +0100

    Library book recall and genre menu

--- Odin project JS/bookTask/bookTask.js
+++ Odin project JS/bookTask/bookTask.js
@@ -1,12 +1,19 @@
-const library = [];
+let library = [];
 
-function book(name,author,pages,read){
+const storedLib = localStorage.getItem("library");
+if(storedLib){
+    const parseLib = JSON.parse(storedLib);
+    library = parseLib.map(book => new Book(book.name, book.author, book.pages, book.genre, book.read))
+}
+
+function Book(name,author,pages,genre,read){
     this.name = name;
     this.author = author;
     this.pages = pages;
+    this.genre = genre;
     this.read = read;
     this.info = function(){
-        return `Name: ${this.name}<br>Author: ${this.author}<br>Pages: ${this.pages}<br>Read: ${this.read}<br><br>`;
+        return `Name: ${this.name}<br>Author: ${this.author}<br>Pages: ${this.pages}<br>Genre: ${this.genre}<br>Read: ${this.read}<br>`;
     }
 }
 
@@ -15,8 +22,9 @@ function addBookToLib(){
     bookList.innerHTML = "";
 
     for (let i=0; i<library.length; i++){
-        bookList.innerHTML += library[i].info() + "<br>";
+        bookList.innerHTML += '<div class="book">' + library[i].info() + "</div>";
     }
+    localStorage.setItem("library", JSON.stringify(library));
 }
 
 
@@ -42,9 +50,10 @@ doneButton.onclick = function(){
     const bookName = document.getElementById("bookName").value;
     const bookAuthor = document.getElementById("bookAuthor").value;
     const bookPages = document.getElementById("bookPages").value;
+    const bookGenre = document.getElementById("bookGenre").value;
 
-    if(bookName!="" && bookAuthor!="" && bookPages!=""){
-        this.book = new book(bookName, bookAuthor, bookPages, radioRead)
+    if(bookName!="" && bookAuthor!="" && bookPages!="" && bookGenre!=""){
+        this.book = new book(bookName, bookAuthor, bookPages, bookGenre, radioRead)
         library.push(this.book)
         addBookToLib();
     }
@@ -52,10 +61,11 @@ doneButton.onclick = function(){
     document.getElementById("bookName").value = "";
     document.getElementById("bookAuthor").value = "";
     document.getElementById("bookPages").value = "";
+    document.getElementById("bookGenre").value = "";
 };
 
 
-const book1 = new book("Ready Player One", "Ernest Cline", 200, "Yes");
-library.push(book1);
+/*const book1 = new book("Ready Player One", "Ernest Cline", 200, "Fantasy/Sci-fi", "Yes");
+library.push(book1);*/
 
 addBookToLib();
