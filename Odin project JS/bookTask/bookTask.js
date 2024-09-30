b271173092e13bc59bb88998aef4e61caa29cb55
Matt Adam <95365935+minimatt25@users.noreply.github.com>
2024-09-30 10:59:20 +0100

    Library star rating and book saving

--- Odin project JS/bookTask/bookTask.js
+++ Odin project JS/bookTask/bookTask.js
@@ -5,18 +5,19 @@ let library = [];
 const storedLib = localStorage.getItem("library");
 if(storedLib){
     const parseLib = JSON.parse(storedLib);
-    library = parseLib.map(book => new Book(book.name, book.author, book.pages, book.genre, book.read))
+    library = parseLib.map(book => new Book(book.name, book.author, book.pages, book.genre, book.read, book.stars))
 }
 
 //making of book
-function Book(name,author,pages,genre,read){
+function Book(name,author,pages,genre,read,stars){
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
     this.read = read;
+    this.stars = stars;
     this.info = function(){
-        return `Name: ${this.name}<br>Author: ${this.author}<br>Pages: ${this.pages}<br>Genre: ${this.genre}<br>Read: ${this.read}<br>`;
+        return `Name: ${this.name}<br>Author: ${this.author}<br>Pages: ${this.pages}<br>Genre: ${this.genre}<br>Read: ${this.read}<br>Stars: ${this.stars}`;
     }
 }
 
@@ -59,15 +60,43 @@ bookButton.onclick = function(){
     document.getElementById("quiz").style.display = "block";
 };
 
+//check if stars section needed
+function starsNeeded(){
+    const readYes = document.getElementById("read").checked;
+    document.getElementById("starStuff").style.display = readYes ? "flex":"none";
+}
+
+//event listener always checks a change in the radio button selected and calls starsNeeded
+document.querySelectorAll('input[name="quiz"]').forEach(radio => {radio.addEventListener("change", starsNeeded)});
+
 //user done with quiz and all data is made into new book
 const doneButton = document.getElementById("done");
 doneButton.onclick = function(){
     document.getElementById("quiz").style.display = "none";
 
     let radioRead = "";
+    let stars = "unrated";
 
     if(document.getElementById("read").checked){
         radioRead = "Yes";
+        
+        let starList = document.getElementsByName("rating");
+        for(let i=0; i<starList.length; i++){
+            if(starList[i].checked){
+                //stars = starList[i].value;
+                stars = `<div class="boxStars"><div class="stars">`
+                for(let j=0; j<starList[i].value; j++){
+                    stars += `<div class="fa fa-star"></div>`
+                }
+                if(starList[i].value!=5){
+                    for(let x=0; x<(5-starList[i].value); x++){
+                        stars += `<div class="fa fa-star" style="color: #838383"></div>`
+                    }
+                }
+                stars += "</div></div>"
+                break;
+            }
+        }
     } else if (document.getElementById("reading").checked){
         radioRead = "Reading";
     } else{
@@ -80,7 +109,7 @@ doneButton.onclick = function(){
     const bookGenre = document.getElementById("bookGenre").value;
 
     if(bookName!="" && bookAuthor!="" && bookPages!="" && bookGenre!=""){
-        this.book = new Book(bookName, bookAuthor, bookPages, bookGenre, radioRead)
+        this.book = new Book(bookName, bookAuthor, bookPages, bookGenre, radioRead, stars)
         library.push(this.book)
         addBookToLib();
     }
@@ -91,4 +120,5 @@ doneButton.onclick = function(){
     document.getElementById("bookGenre").value = "";
 };
 
+starsNeeded();
 addBookToLib();
